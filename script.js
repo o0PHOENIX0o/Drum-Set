9b0c4a302c9e221baf5eaedfdcb648fe9f34f77b
Ujjwal <100501442+o0PHOENIX0o@users.noreply.github.com>
2024-06-25 13:40:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function PlaySound(b){
+    var button = document.querySelector(`.${b}`);
+    var PressedButton = button.innerText;
+    button.classList.add('pressed');
+    PlayAudio(b);
+    
+
+    setTimeout(() => {
+      button.classList.remove('pressed');
+    },1000);
+
+    
+}
+
+function PlayAudio(AudioFile){
+    new Audio(`sounds/${AudioFile}.mp3`).play();
+
+}
+
+
+document.addEventListener('keydown', function(event) {
+    console.log('Key pressed:', event.key);
+    var key = event.key.toLowerCase(); 
+    
+    switch (key) {
+      case 'w':
+        PlaySound('w');
+      case 'a':
+        PlaySound('a');
+        break;
+      case 's':
+        PlaySound('s');
+        break;
+      case 'd':
+        PlaySound('d');
+        break;
+      case 'j':
+        PlaySound('j');
+        break;
+      case 'k':
+        PlaySound('k');
+        break;
+      case 'l':
+        PlaySound('l');
+        break;
+      default:
+        break;
+    }
+  });
