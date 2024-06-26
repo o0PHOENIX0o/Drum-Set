babbdb79aab3c44e2fc926c4c6c35c0594599a99
Phoenix <pheonixyt5@gmail.com>
2024-06-26 18:13:24 +0530

    new js

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
+var audioUrl = {
+  'w': 'sounds/tom-1.mp3',
+  'a': 'sounds/tom-2.mp3',
+  's': 'sounds/tom-3.mp3',
+  'd': 'sounds/tom-4.mp3',
+  'j': 'sounds/snare.mp3',
+  'k': 'sounds/crash.mp3',
+  'l': 'sounds/kick-bass.mp3'
+};
+
+
+function playAudio(key){
+  if (audioUrl[key]) {
+      var button = document.querySelector("."+key);
+      button.classList.add("pressed");
+      new Audio(audioUrl[key]).play();
+      
+      setTimeout(()=> {
+          button.classList.remove("pressed");
+      },400);
+  }
+
+
+}
+
+
 var drums = document.querySelectorAll(".drum");
-for(var i = 0; i<drums.length; i++){
+for (var i = 0; i < drums.length; i++) {
   drums[i].addEventListener('click', function() {
-    Clicked(this.innerText);
+      var buttonKey = this.innerText.toLowerCase(); 
+      playAudio(buttonKey);
   });
 }
 
-document.addEventListener('keydown', function(event) {
-  var key = event.key.toLowerCase(); 
-  if(['w','a','s','d','j','k','l'].includes(key)){
-    Clicked(key);
-  }
+document.addEventListener('keydown', function (event) {
+  playAudio(event.key);
 });
-
-
-
-
-function Clicked(buttonClass){
-  var button = document.querySelector(`.${buttonClass}`);
-  button.classList.add('pressed');
-  PlayAudio(buttonClass);
-
-  setTimeout(() => {
-    button.classList.remove('pressed');
-  },800);
-}
-
-function PlayAudio(AudioFile){
-  new Audio(`sounds/${AudioFile}.mp3`).play();
-}
-
