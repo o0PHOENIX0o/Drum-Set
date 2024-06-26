3288a9d4ff1e82c95b7e64fb140455dbcf7e08c7
Phoenix <pheonixyt5@gmail.com>
2024-06-26 18:41:36 +0530

    js object

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 var audioUrl = {
-  'w': 'sounds/tom-1.mp3',
-  'a': 'sounds/tom-2.mp3',
-  's': 'sounds/tom-3.mp3',
-  'd': 'sounds/tom-4.mp3',
-  'j': 'sounds/snare.mp3',
-  'k': 'sounds/crash.mp3',
-  'l': 'sounds/kick-bass.mp3'
+  w: 'sounds/tom-1.mp3',
+  a: 'sounds/tom-2.mp3',
+  s: 'sounds/tom-3.mp3',
+  d: 'sounds/tom-4.mp3',
+  j: 'sounds/snare.mp3',
+  k: 'sounds/crash.mp3',
+  l: 'sounds/kick-bass.mp3'
 };
 
 
 function playAudio(key){
   if (audioUrl[key]) {
-      var button = document.querySelector("."+key);
-      button.classList.add("pressed");
-      new Audio(audioUrl[key]).play();
-      
-      setTimeout(()=> {
-          button.classList.remove("pressed");
-      },400);
+    var button = document.querySelector("."+key);
+    button.classList.add("pressed");
+
+    new Audio(audioUrl[key]).play();
+    
+    setTimeout(()=> {
+        button.classList.remove("pressed");
+    },400);
   }
-
-
 }
 
 
